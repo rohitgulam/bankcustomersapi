66b92384a1113a62edc6690aac6aeb19e2a8a4cd
rohitgulam <rohitsgulam@gmail.com>
2022-09-12 21:21:59 +0300

    :sparkles: created customer migrations

---
+++ database/migrations/2022_09_11_085655_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('account_type');
+            $table->bigInteger('account_number');
+            $table->integer('balance')->default(0);
+            $table->string('address');
+            $table->string('email');
+            $table->date('dob');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
