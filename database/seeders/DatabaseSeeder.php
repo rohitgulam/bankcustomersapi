224fc8159344a8d66f04b1fab918e437d57fa26d
rohitgulam <rohitsgulam@gmail.com>
2022-09-12 21:22:16 +0300

    created seeder function to seed factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Customer::factory(12)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
